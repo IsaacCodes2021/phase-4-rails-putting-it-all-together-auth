2dad8d0b4d09bf2b04edf13186555599a07e7123
IsaacCodes2021 <84209008+IsaacCodes2021@users.noreply.github.com>
2021-09-09 10:25:05 -0700

    commit tree 9f1dbd7442a7fb44e44650b320a6b5f12f3f4f16

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,24 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+  rescue_from ActiveRecord::RecordNotFound, with: :user_not_exist
+
+  before_action :authorize
+
+  def user_not_exist
+    render json: {errors: "user not found"}, status: 401
+  end
+
+  private 
+  def authorize
+    @current_user = User.find_by(id: session[:user_id])
+
+    render json: { errors: ["Not authorized"] }, status: :unauthorized unless @current_user
+  end
+
+  def render_unprocessable_entity_response(exception)
+    render json: { errors: exception.record.errors.full_messages }, status: :unprocessable_entity
+  end
+
 
 end
